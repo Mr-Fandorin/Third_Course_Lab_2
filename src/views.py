6002fdb5e1062835fe962f8e56944e81a9d3b9df
Sergey Mosykin <smosyakin@yandex.ru>
2025-11-29 14:54:24 +0300

    fixing views.py

--- src/views.py
+++ src/views.py
@@ -12,14 +12,14 @@ from src.utils import (
 )
 
 
-def get_info_by_request(request_date):
+def get_info_by_request(request_date, file_data="../data/output.json"):
     """Функция по переданной дате выдает транзакции за период от начала месяца до введенной даты,
     также сообщает текщий курс по валютам из файла пользователя,
     также выдает стоимость акций по компаниям из файла пользователя"""
     print(get_current_time())  # Приветствие согласно текущему времени суток
     dict_hi = {}
-    dict_hi["greeting"] = "Добрый день"  # Запись приветствия в файл JSON
-    with open("../data/output.json", "w", encoding="utf-8") as f:
+    dict_hi["greeting"] = get_current_time()  # Запись приветствия в файл JSON
+    with open(file_data, "w", encoding="utf-8") as f:
         json.dump(dict_hi, f, ensure_ascii=False)
     list_transaction = reader_excel_transaction("../data/operations.xlsx")  # Загрузка информации из файла Excel
     period_list_transactions = get_period_transactions(list_transaction, request_date)  # Загрузка данных по периоду
@@ -28,10 +28,10 @@ def get_info_by_request(request_date):
     user_data = get_setting("../data/user_settings.json")  # Считывание данных из JSON-файла пользователя
     user_rates = get_exchange_rate(user_data)  # Запрос курса валют по API
     user_stocks = get_share_price(user_data)  # Запрос стоимости акций по API
-    with open("../data/output.json", "r", encoding="utf-8") as f:  # Чтение итогового файла JSON со всеми данными
+    with open(file_data, "r", encoding="utf-8") as f:  # Чтение итогового файла JSON со всеми данными
         existing_data = json.load(f)
 
     return existing_data
 
 
-# print(get_info_by_request("04-01-2018 15:00:41"))
+print(get_info_by_request("04-01-2018 15:00:41"))
